b96630f204e94db3c3c0dde6f8b33ed604dcc118
ahnafabir <ahnafabir313@gmail.com>
2024-05-01 13:55:34 +0600

    created static project timeline page

---
+++ src/app/(dashboard)/projects/[id]/timeline/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+import { Timeline, Typography, Space } from "antd";
+import React from "react";
+
+export default function ProjectTimeline() {
+  return (
+    <main>
+      <Typography.Title level={3} className="!mb-8">
+        Project Timeline
+      </Typography.Title>
+
+      <Timeline
+        mode="alternate"
+        items={[
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+          {
+            children: (
+              <Space direction="vertical">
+                <Typography.Text mark>01 May 2023 11:30</Typography.Text>
+                <Typography.Text>
+                  New Task Created by{" "}
+                  <Typography.Text code>Ahnaf Abir</Typography.Text>
+                </Typography.Text>
+              </Space>
+            ),
+          },
+        ]}
+      />
+    </main>
+  );
+}
